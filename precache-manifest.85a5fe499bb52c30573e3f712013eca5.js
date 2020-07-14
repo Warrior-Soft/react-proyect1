704dbd28501a7b9919bd8d4911d32328c1f3d1da
Warrior-Soft <boss280498@gmail.com>
2020-07-14 18:48:04 -0400

    Updates

---
+++ precache-manifest.85a5fe499bb52c30573e3f712013eca5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f089a48d7b5ed5859771eccb2c175983",
+    "url": "/react-proyect1/index.html"
+  },
+  {
+    "revision": "397e08c70eab0fe93170",
+    "url": "/react-proyect1/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d7d4a646e4008334a04e",
+    "url": "/react-proyect1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "397e08c70eab0fe93170",
+    "url": "/react-proyect1/static/js/2.d8e7ad36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-proyect1/static/js/2.d8e7ad36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d4a646e4008334a04e",
+    "url": "/react-proyect1/static/js/main.3efdd015.chunk.js"
+  },
+  {
+    "revision": "a6b49494ac9037679b75",
+    "url": "/react-proyect1/static/js/runtime-main.ea421258.js"
+  },
+  {
+    "revision": "9cd06b907e9158f8cd7abda692200103",
+    "url": "/react-proyect1/static/media/foto.9cd06b90.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-proyect1/static/media/logo.5d5d9eef.svg"
+  }
+]);
